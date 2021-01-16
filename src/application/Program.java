8339d47c2b1d28a7820ba24272eeb6dd559227df
Eder Sant'ana <edersana@gmail.com>
2021-01-16 12:16:43 -0300

    Department and Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,13 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		
-		DB.closeConnection();
+		Department depart = new Department(1, "Books");
+		System.out.println(depart);
 		
 	}
 
