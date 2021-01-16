3a1930922c259328c9165fe2819bf530c9bc7e96
Eder Sant'ana <edersana@gmail.com>
2021-01-16 12:56:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -23,6 +25,8 @@ public class Program {
 		catch(ParseException e) {
 			System.out.println(e.getMessage());
 		}
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
