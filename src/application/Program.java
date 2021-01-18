62e6e7676f2f5e7587008fad86c00d01cfca60f6
Eder Sant'ana <edersana@gmail.com>
2021-01-18 12:50:51 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -53,6 +53,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerdao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
+			
+		System.out.println("==== TEST 5: update seller ====");
+		
+		seller = sellerdao.findById(1);
+		seller.setName("Bruce Wayne");
+		sellerdao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
