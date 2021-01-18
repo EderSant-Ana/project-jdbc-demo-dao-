62bf25c8eda8fceaeec053339ef72d2bd316d494
Eder Sant'ana <edersana@gmail.com>
2021-01-18 13:26:55 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
@@ -61,6 +64,16 @@ public class Program {
 		seller.setName("Bruce Wayne");
 		sellerdao.update(seller);
 		System.out.println("Update completed");
-	}
+		System.out.println();
+		
+		System.out.println("==== TEST 6: delete seller ====");
 
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerdao.deleteById(id);
+		System.out.println("Delete completed");
+		System.out.println();
+		
+		sc.close();
+	}
 }
